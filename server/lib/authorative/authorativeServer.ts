968a68c0691c64be5cc99af9edc2d035d66a6b6e
Tzook <tzook10@gmail.com>
2019-06-09 10:57:30 +0300

    Remove pipes

--- server/lib/authorative/authorativeServer.ts
+++ server/lib/authorative/authorativeServer.ts
@@ -4,9 +4,6 @@ export function start() {
     console.log("~~~ Starting authorative server ~~~");
     setTimeout(() => {
         console.log("~~~ Timer ~~~");
-        const unityProcess = spawn("build/cvd.x86_64", {detached: true});
-
-        unityProcess.stdout.pipe(process.stdout);
-        unityProcess.stderr.pipe(process.stderr);
+        spawn("build/cvd.x86_64", {detached: true});
     }, 5000);
 }
